f4b5613c74353ed690febff53408e68edd2b2a6a
jesseflores512 <jesseflores512@gmail.com>
2018-05-10 21:24:54 -0500

    portfolio layout is done

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Project from '../subcomponents/project'
-import Grid from 'react-css-grid'
 
 export default class Portfolio extends React.Component {
   render(){
--- src/subcomponents/project.js
+++ src/subcomponents/project.js
@@ -5,11 +5,12 @@ export default class Project extends React.Component {
     return(
         <div>
           <div className='project'>
-            
+            <img src={require('../images/projectimages/rep.png')} alt='project'></img>
             <p className='projectText'>
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
+
           </div>
         </div>
     )
