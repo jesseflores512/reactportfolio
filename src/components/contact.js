cd6cd7a28c9b24ebed3ce9c38f56f1a1605eec4d
jesseflores512 <jesseflores512@gmail.com>
2018-05-10 22:07:34 -0500

    added to contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,6 +7,11 @@ export default class Contact extends React.Component {
           <div className='contactPage'>
             <div className='contact'>
               <h1>Contact</h1>
+              <p>
+                Email: <a email='jesseflores512@gmail.com' className='contacts'>jesseflores512@gmail.com</a><br/>
+                Phone: 512-573-5339 <br/>
+                Github: <a href='https://github.com/jesseflores512' className='contacts'>Click to view</a>
+              </p>
             </div>
           </div>
         </div>
