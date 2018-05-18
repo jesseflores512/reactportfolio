0de37590a911f87fd18534b44c629f3846917c39
jesseflores512 <jesseflores512@gmail.com>
2018-05-17 20:30:18 -0500

    added new component to portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -5,17 +5,24 @@ export default class Portfolio extends React.Component {
   render(){
     const test = 'test'
     const rep = {
-      text : 'This is an app made with basic HTML/CSS/JS. Using 2 google APIs it finds your representatives based on your location',
+      text : 'This app finds your representatives based on your location, using 2 google APIs. Made with HTML/CSS/JS/JQuery',
       image : require('../images/projectimages/rep.png'),
       link : 'https://arnoldrz2.github.io/Know-Your-Rep/'
+    };
+
+    const goals = {
+      text : 'Made this simple webpage to learn CSS grids. Made with HTML/CSS/JS/JQuery',
+      image : require('../images/projectimages/goals.png'),
+      link : 'https://jesseflores512.github.io/goals/'
     };
     return(
         <div>
           <div className='portfolio'>
+              <Project text = {goals.text} image = {goals.image} link ={goals.link}/>
               <Project text = {rep.text} image = {rep.image} link ={rep.link}/>
               <Project text = {test}/>
               <Project text = {test}/>
-              <Project text = {test}/>
+
           </div>
         </div>
     )
