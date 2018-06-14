186010e874ec67798a07105f4e9afe022e065aba
jesseflores512 <jesseflores512@gmail.com>
2018-06-13 19:47:48 -0500

    css changes for mobile

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,10 +8,10 @@ export default class Contact extends React.Component {
             <div className='contact'>
               <h1>Contact</h1>
               <p>
-                Email: <a email='jesseflores512@gmail.com' className='contacts'>jesseflores512@gmail.com</a><br/>
+                Email: <a href='mailto: jesseflores512@gmail.com' className='contacts'>jesseflores512@gmail.com</a><br/>
                 Phone: 512-573-5339 <br/>
-                Github: <a href='https://github.com/jesseflores512' className='contacts'>Click to view</a><br/>
-                LinkedIn: <a href='https://www.linkedin.com/in/jesseflores512/' className='contacts'>Click to view</a>
+                Github: <a href='https://github.com/jesseflores512' className='contacts' target="_blank">Click to view</a><br/>
+                LinkedIn: <a href='https://www.linkedin.com/in/jesseflores512/' className='contacts' target="_blank">Click to view</a>
               </p>
             </div>
           </div>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -5,7 +5,7 @@ export default class Portfolio extends React.Component {
   render(){
     const test = 'test'
     const rep = {
-      text : 'Find your representatives based on your location, using 2 google APIs. Made with HTML/CSS/JS/JQuery',
+      text : 'Find your representatives based on location, using 2 google APIs. Made with HTML/CSS/JS/JQuery',
       image : require('../images/projectimages/rep.png'),
       link : 'https://arnoldrz2.github.io/Know-Your-Rep/'
     };
